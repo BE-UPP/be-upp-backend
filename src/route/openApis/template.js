17aea1f0f4f5069bbf43e778ec6ad2ed1bbe39f9
LeaoLuciano <lucianorsleao@gmail.com>
2021-10-16 11:32:56 -0300

    Updated template schema, routes for post settled, set template service added
    Co-authored-by: Daniel Angelo Esteves Lawand <daniel.lawand@usp.br>
    Co-authored-by: Marcos Siolin Martins <siolinm@usp.br>

--- src/route/openApis/template.js
+++ src/route/openApis/template.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getTemplateById, getLastestTemplate } = require('../../service/template');
+const {
+  getTemplateById,
+  getLatestTemplate,
+  setTemplate,
+} = require('../../service/template');
 
 router.get('/by-id/:id', async (req, res) => {
   const id = req.params ? req.params.id : false;
-  if(!id) {
+  if (!id) {
     // TODO error
   }
 
   try {
     const template = await getTemplateById(id);
     res.send(template);
-  }catch (error){
+  } catch (error) {
     console.log(error)
     // TODO error
     res.send(error.message);
@@ -20,13 +24,24 @@ router.get('/by-id/:id', async (req, res) => {
 
 router.get('/latest', async (req, res) => {
   try {
-    const template = await getLastestTemplate();
+    const template = await getLatestTemplate();
     res.send(template);
-  }catch (error){
+  } catch (error) {
     console.log(error)
     // TODO error
     res.send(error.message);
   }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const template = await setTemplate(req.body);
+    res.send(template);
+  } catch (error) {
+    console.log(error);
+    res.status(error.code).send(error.err.message);
+  }
+
 });
 
 module.exports = router;
--- src/service/template.js
+++ src/service/template.js
@@ -1,11 +1,10 @@
 const Model = require('../data/models/template');
 
-exports.getTemplateById = async (id) => {
+const getTemplateById = async (id) => {
   try {
     const dado = await Model.findById(id).exec();
     return dado;
-  }
-  catch(error) {
+  } catch (error) {
     throw {
       err: error,
       code: 500,
@@ -13,15 +12,44 @@ exports.getTemplateById = async (id) => {
   }
 }
 
-exports.getLastestTemplate = async () => {
+const getLatestTemplate = async () => {
   try {
-    const dado = await Model.findOne().sort({createAt: -1}).exec();
+    const dado = await Model.findOne().sort({
+      createAt: -1
+    }).exec();
     return dado;
-  }
-  catch(error) {
+  } catch (error) {
     throw {
       err: error,
       code: 500,
     }
   }
 }
+
+const setTemplate = async (pages) => {
+  try {
+    const now = Date.now();
+    const latest_template = await getLatestTemplate();
+    const version = latest_template ? latest_template.templateVersion + 1 : 0;
+    const dado = {
+      templateVersion: version,
+      pages: pages,
+      createAt: now,
+    }
+    const result = await Model.create(dado);
+    return result;
+  } catch (error) {
+    throw {
+      err: error,
+      code: 400,
+    }
+  }
+
+}
+
+
+module.exports = {
+  getTemplateById: getTemplateById,
+  getLatestTemplate: getLatestTemplate,
+  setTemplate: setTemplate,
+};
