80a017912a1b508c0019ed78883496f5991cf039
LeaoLuciano <lucianorsleao@gmail.com>
2021-10-16 12:02:48 -0300

    Started API post form data
    Co-authored-by: Marcos Siolin Martins <siolinm@usp.br>
    Co-authored-by: Daniel Angelo Esteves Lawand <daniel.lawand@usp.br>

---
+++ src/service/form-data.js
@@ -0,0 +1,16 @@
+const Model = require('../data/models/form-data');
+
+const addFormData = async (data) => {
+  //TODO
+  try {} catch (error) {
+    throw {
+
+    }
+  }
+
+}
+
+
+module.exports = {
+  addFormData: addFormData
+};
