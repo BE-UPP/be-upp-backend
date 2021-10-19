bc8135e1ca09019ec4638f89dbdd8244c19c0dca
Marcos Siolin Martins <marcossiolinmartins@gmail.com>
2021-10-19 17:57:54 -0300

    Added form-data post route
    Co-authored-by: Daniel Angelo Esteves Lawand <daniel.lawand@usp.br>
    Co-authored-by: Luciano Rodrigues Saraiva Leão <lucianorsleao@usp.br>

--- src/route/openApis/form-data.js
+++ src/route/openApis/form-data.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { addFormData } = require('../../service/form-data');
 
 router.post('/', async (req, res) => {
-  //TODO
+  try {
+    const data = await addFormData(req.body);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(error.code).send(error.err.message);
+  }
 });
 
 module.exports = router;
--- src/service/form-data.js
+++ src/service/form-data.js
@@ -1,10 +1,15 @@
 const Model = require('../data/models/form-data');
 
 const addFormData = async (data) => {
-  //TODO
-  try {} catch (error) {
+  try {
+    const answerAt = Date.now();
+    data.answeredAt = answerAt;
+    const dado = await Model.create(data);
+    return dado;
+  } catch (error) {
     throw {
-
+      err: error,
+      code: 400
     }
   }
 
