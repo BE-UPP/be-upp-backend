3762bcfe5a8d62de15d815e03615adcedcf16601
William Sena Silva <williamsenasilva@yahoo.com>
2021-11-15 00:46:34 -0300

    Corrigido erros apontados pelo circle cli.

--- src/data/mail/mailer.js
+++ src/data/mail/mailer.js
@@ -1,33 +1,33 @@
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
 
 /*  HOW TO USE
-    Para Gmail, precisa habilitar envio para apps menos seguros e desabilitar Captcha
-    https://myaccount.google.com/lesssecureapps
-    https://accounts.google.com/b/0/DisplayUnlockCaptcha
+  Para Gmail, precisa habilitar envio para apps menos seguros e desabilitar Captcha
+  https://myaccount.google.com/lesssecureapps
+  https://accounts.google.com/b/0/DisplayUnlockCaptcha
 
-    EXAMPLE
-    const mailer = require('../data/mail/mailer')
+  EXAMPLE
+  const mailer = require('../data/mail/mailer')
 
-    var mailOptions = {
-        to: "paciente@mail.com",
-        subject: "Olá, paciente! Isto é um teste",
-        html: "<h1>Welcome to be-upp</h1>"
-    }
-    mailer.sendMail(mailOptions, function (error, response) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Mensagem enviada: ", response);
-        }
-    });
-*/ 
+  var mailOptions = {
+      to: "paciente@mail.com",
+      subject: "Olá, paciente! Isto é um teste",
+      html: "<h1>Welcome to be-upp</h1>"
+  }
+  mailer.sendMail(mailOptions, function (error, response) {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log("Mensagem enviada: ", response);
+      }
+  });
+*/
 
 var mailer = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    }
+  service: 'Gmail',
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
 });
 
 module.exports = mailer;
