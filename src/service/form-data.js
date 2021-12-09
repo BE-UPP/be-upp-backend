f29403aee587f1b2c6a1749144abb41c9c153d73
Davi de Menezes Pereira <55624305+davimp@users.noreply.github.com>
2021-12-09 18:02:57 -0300

    Revert "Modelo e serviços do Relatório Final criados (#63)" (#65)
    This reverts commit 14ae7eabd27f3752c0215eb5a3a5404a6e9746e2.

--- src/service/form-data.js
+++ src/service/form-data.js
@@ -1,11 +1,20 @@
 const Model = require('../data/models/form-data');
 const { getAppointmentById } = require('./appointment');
+const { getTemplateByVersion } = require('./template');
 
 const addFormData = async(data) => {
   try {
     const answerAt = Date.now();
     data.answeredAt = answerAt;
     await getAppointmentById(data.appointmentId);
+    let version = data.templateVersion;
+    let template = await getTemplateByVersion(version);
+
+    for (let i = 0; i < data.questions.lenght; i++) {
+      let id = data.questions[i].id;
+      let variables = template['questions'][id]['variables'];
+      data.questions[i].variables = variables;
+    }
 
     const dado = await Model.create(data);
     return dado;
