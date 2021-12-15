301068c821f763c32ce1896e06381b6e6498b857
LeaoLuciano <lucianorsleao@gmail.com>
2021-12-15 16:06:21 -0300

    API de new finalReport agora e closed

--- src/route/openApis.js
+++ src/route/openApis.js
@@ -5,10 +5,12 @@ const templatesRoutes = require('./openApis/template');
 const formDataRoutes = require('./openApis/form-data');
 const doctorRoutes = require('./openApis/doctor');
 const dataProcessingRoutes = require('./openApis/data-processing');
+const finalReportRoutes = require('./openApis/final-report');
 
 router.use('/template', templatesRoutes);
 router.use('/form-data', formDataRoutes);
 router.use('/doctor', doctorRoutes);
 router.use('/data-processing', dataProcessingRoutes);
+router.use('/final-report', finalReportRoutes);
 
 module.exports = router;
