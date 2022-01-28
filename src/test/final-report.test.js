1284f734481ab556b338bad391475d0aa07a55b1
LeaoLuciano <lucianorsleao@gmail.com>
2022-01-28 02:54:40 -0300

    final report sem versão e sobrescrevendo

--- src/test/final-report.test.js
+++ src/test/final-report.test.js
@@ -4,6 +4,9 @@ const { addFinalReportTemplate, getFinalReportTemplateByVersion,
 const { createNewPatient } = require('../service/patient');
 const { createNewDoctor, validateDoctorLogin } = require('../service/doctor');
 const { createNewAppointment } = require('../service/appointment');
+const {
+  setTemplate,
+} = require('../service/template');
 const { app,
   openServer, closeServer } = require('../server');
 const { clone } = require('../service/helper');
@@ -118,6 +121,67 @@ describe('Testing FinalReport services', () => {
     done();
   });
 
+  it('create new FinalReportTemplate without version', async(done) => {
+
+    let FinalReportTemplate2 = {
+      pages: [
+        {
+          pageLabel: 'Dados Gerais',
+          values: [
+            'nome',
+            'sexo',
+            'idade',
+          ],
+          items: [
+            {
+              label: 'Nome:',
+              type: 'text',
+              content: [
+                0,
+              ],
+            },
+          ],
+        },
+
+      ],
+    };
+
+    let pages = [
+      {
+        pageLabel: 'A page label',
+        questions: {
+          name: {
+            questionLabel: 'Nome Incompleto',
+            placeholder: 'Ex: José Fernando da Silva',
+            type: 'text',
+            variables: ['name'],
+          },
+        },
+      },
+    ];
+
+
+    await setTemplate(pages);
+    await setTemplate(pages);
+    await setTemplate(pages);
+    await setTemplate(pages);
+
+    let FRTemplate = await addFinalReportTemplate(FinalReportTemplate2);
+    let output = clone(FinalReportTemplate2);
+    output.isTemplate = true;
+    output.version = 3;
+
+    let t = clone(FRTemplate);
+    delete t._id;
+    delete t.__v;
+
+    for (let i in t.pages)
+      delete t.pages[i]._id;
+
+    expect(t).toEqual(output);
+    done();
+  });
+
   it('get FinalReportTemplate', async(done) => {
     let FRTemplate = await addFinalReportTemplate(FinalReportTemplate);
     let t = clone(FRTemplate);
@@ -169,19 +233,64 @@ describe('Testing FinalReport services', () => {
     done();
   });
 
+  it('Overwrite FinalReportData', async(done) => {
+    const pat = await createNewPatient(p.name, p.email, p.cpf, p.cellphone,
+      p.birth, p.password);
+    const doc = await createNewDoctor(d.name, d.email, d.password, d.cellphone,
+      d.phone, d.rcn);
+    const date = Date.now();
+    let t3 = await createNewAppointment(date, pat._id, doc._id);
+    let formData = clone(answers);
+    formData.appointmentId = t3._id;
+    let formData2 = {
+      questions: [
+        {
+          id: 'nome',
+          values: ['José da Silva2'],
+        },
+        {
+          id: 'sexo',
+          values: ['Masculino2'],
+        } ],
+      templateVersion: 1,
+    };
+    formData2.appointmentId = t3._id;
+
+    await addFinalReportTemplate(FinalReportTemplate);
+    await addFinalReportData(formData2, variables);
+    let FRData = await addFinalReportData(formData, variables);
+    let output = clone(FinalReportTemplate);
+    output.appointmentId = String(t3._id);
+    delete output.isTemplate;
+
+    output.pages[0].values = [
+      'José da Silva',
+      'Masculino',
+      '90' ];
+
+    let t2 = clone(FRData);
+    delete t2._id;
+    delete t2.__v;
+    for (let i in t2.pages)
+      delete t2.pages[i]._id;
+
+    expect(t2).toEqual(output);
+    done();
+  });
+
   it('get new FinalReportData', async(done) => {
     const pat = await createNewPatient(p.name, p.email, p.cpf, p.cellphone,
       p.birth, p.password);
     const doc = await createNewDoctor(d.name, d.email, d.password, d.cellphone,
       d.phone, d.rcn);
     const date = Date.now();
-    const t = await createNewAppointment(date, pat._id, doc._id);
+    let t2 = await createNewAppointment(date, pat._id, doc._id);
     let formData = clone(answers);
-    formData.appointmentId = t._id;
+    formData.appointmentId = t2._id;
 
     await addFinalReportTemplate(FinalReportTemplate);
     let FRData = await addFinalReportData(formData, variables);
-    let FRData2 = await getFinalReportData(t._id);
+    let FRData2 = await getFinalReportData(t2._id);
 
     expect(FRData2.toObject()).toEqual(FRData.toObject());
     done();
