5da65dc3a323a7e9af93942bb700d2c08da4cb7f
siolinm <36745419+siolinm@users.noreply.github.com>
2022-04-18 22:51:25 -0300

    Reprocessamento de relatório pelo admin (#108)

--- src/route/closeApis/final-report.js
+++ src/route/closeApis/final-report.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const {
   getFinalReportData,
 } = require('../../service/final-report');
+const {
+  getFormDataByAppointmentId,
+} = require('../../service/form-data');
+const { addFinalReportData } = require('../../service/final-report');
+const { getVariables } = require('../../service/template');
+const { processData } = require('../../service/data-processing');
 const { authorize } = require('../../service/authentication');
-
 
 router.get('/by-id', authorize(), async(req, res) => {
   try {
@@ -20,6 +25,30 @@ router.get('/by-id', authorize(), async(req, res) => {
     const frData = await getFinalReportData(id);
 
     res.send(frData);
+  } catch (error) {
+    console.log(error);
+    // TODO error
+    res.status(error.code).send(error.message);
+  }
+});
+
+router.get('/recompute', authorize('admin'), async(req, res) => {
+  try {
+    const id = req.query ? req.query.id : false;
+    if (!id) {
+      throw Object.assign(
+        new Error('Ausência de valores (requerido: appointmentId)'),
+        { code: 402 },
+      );
+    }
+
+    const formData = await getFormDataByAppointmentId(id);
+    const variables = await getVariables(formData);
+    const variables2 = await processData(formData, variables);
+
+    const data = await addFinalReportData(formData, variables2);
+
+    res.send(data);
   } catch (error) {
     console.log(error);
     // TODO error
